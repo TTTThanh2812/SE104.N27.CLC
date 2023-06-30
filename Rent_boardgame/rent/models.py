ce2308a684eb74735d06205c6aa1fd4f47b778c6
TTTThanh <thanhbmt0902@gmail.com>
2023-07-01 02:01:29 +0700

    link với frontend, bug
    link backend với frontend, bug khi người dùng đã gửi yêu cầu, vẫn gửi được yêu cầu tiếp theo, chưa có đk boardgame khi boardgame đang được đặt chỗ

--- Rent_boardgame/rent/models.py
+++ Rent_boardgame/rent/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from userauths.models import User
 from django.utils import timezone
+from decimal import Decimal
 
 STATUS_CHOICES = [
         ('pending', 'Pending'),
@@ -21,8 +22,8 @@ class RentBoardgame(models.Model):
     end_date = models.DateField()
     boardgame_numbers = models.ForeignKey('boardgame.BoardgameNumbers', on_delete=models.CASCADE)
     rental_price = models.DecimalField(max_digits=8, decimal_places=2)
-    deposit_price = models.DecimalField(max_digits=8, decimal_places=2)
-    total_price = models.DecimalField(max_digits=8, decimal_places=2)
+    # deposit_price = models.DecimalField(max_digits=8, decimal_places=2)
+    # total_price = models.DecimalField(max_digits=8, decimal_places=2)
     created_at = models.DateTimeField(default=timezone.now)  
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
     rental_status = models.CharField(max_length=20, choices=RENTAL_STATUS_CHOICES, blank=True)
@@ -38,11 +39,11 @@ class RentBoardgame(models.Model):
         rental_price_per_day = self.boardgame_numbers.boardgame.rental_price()
         return rental_days * rental_price_per_day
     
-    def calculate_deposit_price(self):
-        return self.rental_price * 0.5
+    # def calculate_deposit_price(self):
+    #     return self.rental_price * Decimal(0.5)
     
-    def calculate_total_price(self):
-        return self.rental_price + self.deposit_price
+    # def calculate_total_price(self):
+    #     return self.rental_price + self.deposit_price
     
     def is_expired(self):
         return self.rental_status == 'active' and self.end_date < timezone.now().date()
