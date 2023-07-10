9f7ede4df4151a5539985f719bbe3b1e15a8fa56
TTTThanh <thanhbmt0902@gmail.com>
2023-07-10 21:28:04 +0700

    BE- cập nhập trạng thái thuê của booardgame, cài thư viện thông báo của django để thực hiện thông báo cho người dùng khi boardgame quá hạn
    trạng thái thuê của boardgame bao gồm: đang thuê, đã trã, trễ.
    Cập nhập trại thái thuê, khi admin chọn accepted cho trạng thái yêu cầu trạng trái thuê chuyển thành active(đang thuê), nếu ngày hiện tại lớn hơn ngày hết hạn của boardgame chuyển trạng thái thuê sang trạng thái trễ, khi trạng thái thuê được xác nhận đã trã, trả trạng thái của boardgame_number về trạng thái đang trong kho
    Thông báo tới người dùng boardgame quá hạn khi ngày hiện tại lớn lớn hơn ngày hết hạn boardgame (chỉ cài điều kiện, chưa cài đặt gửi tới người dùng)

--- Rent_boardgame/Rent_boardgame/settings.py
+++ Rent_boardgame/Rent_boardgame/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'boardgame',
     'userauths',
     'rent',
+    'notifications',
 ]
 
 MIDDLEWARE = [
--- Rent_boardgame/rent/models.py
+++ Rent_boardgame/rent/models.py
@@ -3,6 +3,7 @@ from userauths.models import User
 from django.utils import timezone
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from notifications.signals import notify
 
 ORDER_STATUS_CHOICES = [
         ('pending', 'Pending'),
@@ -22,8 +23,7 @@ class RentBoardgame(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
     boardgame_numbers = models.ForeignKey('boardgame.BoardgameNumbers', 
-                                          on_delete=models.CASCADE,
-                                          limit_choices_to={'boardgame_number_status': 'order'})
+                                          on_delete=models.CASCADE)
     rental_price = models.DecimalField(max_digits=8, decimal_places=2)
     deposit_price = models.DecimalField(max_digits=8, decimal_places=2)
     total_price = models.DecimalField(max_digits=8, decimal_places=2)
@@ -38,10 +38,25 @@ class RentBoardgame(models.Model):
     def boardgame_rent_id(self):
         return f"{self.boardgame_numbers.boardgame.category.cid}{self.boardgame_numbers.boardgame.version.vid}{self.boardgame_numbers.boardgame.bgid}{self.boardgame_numbers.bgnid}"
 
-
+    def send_expired_notification(self):
+        user = self.renter
+        message = "Your rental has expired."
+        notify.send(self, recipient=user, verb=message)
     
-    def is_expired(self):
-        return self.rental_status == 'active' and self.end_date < timezone.now().date()
+    def update_rental_status(self):
+        now = timezone.now().date()
+        if self.rental_status == 'active':
+            if now > self.end_date:
+                self.rental_status = 'expired'
+        elif self.rental_status == 'replied':
+            self.boardgame_numbers.boardgame_number_status = 'in_stock'
+        elif self.order_status == 'accepted':
+            self.rental_status = 'active'
+
+    def save(self, *args, **kwargs):
+        self.update_rental_status()
+        super().save(*args, **kwargs)
+    
 
     def __str__(self):
         return f"{self.renter.username} - {self.boardgame_numbers.boardgame.title} - {self.rid}"
@@ -63,6 +78,7 @@ def update_boardgame_stats_on_order_status(sender, instance, **kwargs):
         # rental.rental_status = 'Active'
 
     boardgame_number.boardgame.total = boardgame_number.boardgame.in_stock + boardgame_number.boardgame.order + boardgame_number.boardgame.rental
+
     boardgame_number.boardgame.save()
 
 @receiver(post_save, sender=RentBoardgame, dispatch_uid="update_boardgame_number_stats_on_order_status")
